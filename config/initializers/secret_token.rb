cac2e35c64ec4f2cc04562582dc710683953e5ab
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-01-08 11:58:31 -0200

    inicial project openmarket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openmarket::Application.config.secret_key_base = '71d2324da15cb51fc28c62de65c40c71dba04a87b924bf4b738bbbedce330faf89f24a8d0fb90eda0e91d2c677e4916c83966b297749481c1db3ed3e09e1b3c7'
